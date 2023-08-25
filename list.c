c1522fea4935e6042c9043cac7144a126d410883
Replit user <>
2023-08-25 22:03:11 +0000

    new try

--- list.c
+++ list.c
@@ -63,7 +63,7 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
-  Node *newNode = createNode(data)
+  Node *newNode = createNode(data);
 }
 
 void pushBack(List * list, void * data) {
